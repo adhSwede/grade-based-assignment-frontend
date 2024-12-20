1ae28acfee63da1728b1c214b62ffa20a7b7db90
adhSwede <119529943+adhSwede@users.noreply.github.com>
2024-12-20 16:19:46 +0100

    file path corrections

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,7 +11,7 @@ import {
   getDetailedInfo,
   insertDetailsToDOM,
   selectSearchType,
-} from "/assets/scripts/utilities.js";
+} from "/utilities.js";
 
 // ===========================================================
 //        Start Page
